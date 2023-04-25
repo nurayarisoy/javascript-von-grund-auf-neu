fe67345fc05efb2885c0fa02673f38130c1f2e2a
Nuray arisoy <cevriyeceven@gmail.com>
2023-04-25 12:44:49 +0200

    first commit

---
+++ Fundamentals/Console/app.js
@@ -0,0 +1,12 @@
+console.log('merhaba javascript');
+console.log(1324358);
+console.log(true)
+console.log(false)
+
+var a =10
+console.log(a)
+console.log(['ayse','fatma','zeynep'])
+console.log(typeof a);
+console.warn('bu uyari');
+console.error('bu sayfa bulunamadi')
+
---
+++ Fundamentals/Java script Beginning/app.js
@@ -0,0 +1,3 @@
+alert('hosgeldin')
+var a =10
+alert(a)
