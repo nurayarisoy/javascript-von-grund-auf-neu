fb2e78659009a7cd9f2c73f00f11cf23c73e52f7
Nuray arisoy <cevriyeceven@gmail.com>
2023-04-25 21:34:36 +0200

    add math

---
+++ Fundamentals/String Metod/app.js
@@ -0,0 +1,32 @@
+let value;
+
+const firstName = "ayse";
+const lastName = "ari";
+
+const langs = "java ,python, react";
+/*value = firstName + lastName;
+value = firstName + " " + lastName;
+value = "nuray cevriye";
+value += "arisoy"; //value =value+'arisoy'
+value = firstName.length
+value = firstName.concat(' ',lastName,' ','caz')// uc uca ekler
+value = firstName.toLowerCase()//kucult
+value = firstName.toUpperCase()// buyut
+
+vavalue = firstName[0]//birinci index alir
+value = firstName[2]//ikinci index alir
+
+//index of
+vavalue = firstName.indexOf('a')// a nin kacinci index oldugunu verir.
+// char At
+vavalue = firstName.charAt(0);*///ilk elemani alir
+//split: virgulle ayrilan herbir elemani ayirip alacak
+value = langs.split(',');
+
+//replace: string yerini degistirmek icin kullanilir
+value =langs.replace('react','python') // react yerine pytohon yaz
+// includes
+value =langs.includes('python') // sorguluyon varmi diye
+
+
+console.log(value);
