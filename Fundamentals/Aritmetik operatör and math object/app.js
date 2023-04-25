83eb9b605b1f413ecf78758249c74af939a120e8
Nuray arisoy <cevriyeceven@gmail.com>
2023-04-25 20:56:43 +0200

    add math

---
+++ Fundamentals/Aritmetik operatör and math object/app.js
@@ -0,0 +1,34 @@
+let value;
+const value1=10;
+const value2 =4;
+//aritmetik operatör
+//value=value1 + value2;
+//value=value1 - value2;
+//value=value1 * value2;
+//value=value1 / value2;
+//value=value1 % value2;
+ value = Math.PI
+ value =Math.E
+ value =Math.round(3.6)//4 yuvarar
+ value =Math.round(3.5)// 4 yuvarlar
+ value =Math.round(3.2)// 3 yuvarlar
+
+
+ value =Math.ceil(3.2) // yukari yuvarlar yani 4 olur
+ value =Math.ceil(3.7)
+
+ value =Math.floor(3.2)// asagi yuvarlar yani 3olur
+
+ value =Math.floor(3.9)
+ value= Math.sqrt(16); // karaköku
+ value= Math.abs(-10);// mutlak deger
+ value= Math.pow(8,3);// ustlu sayisi
+
+ value= Math.max(10,2,-3,50);// arasindaki enbuyuk degeri
+ value= Math.min(10,2,-3,50);// enkucuk degeri
+ value= Math.random();//0 ile 1 arasinda ondalikli deger uretecek
+ value= Math.floor(Math.random()* 20 +1);//0 ile bir arasi *20 +1 sonra flood ie asagi yuvarla
+ // get rondom number javascrip   () method  ara
+ //yada mozilla bakabilirsin
+ 
+ console.log(value)
