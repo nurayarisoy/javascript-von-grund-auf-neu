81e56d33f5ef7622e7616e2cfcd9ceaacb64ed97
Nuray arisoy <cevriyeceven@gmail.com>
2023-04-25 13:34:51 +0200

    add let var const

---
+++ Fundamentals/variable var,let,const/app.js
@@ -0,0 +1,31 @@
+// var
+/*var a; // burda  degiskeni tanimlaya biliriz,  yada
+a= 20 // burda ayreten tanimlaya biliriz
+console.log(a)
+ var name='nuray';
+ console.log(name);
+ var name='ayse';// yani yenide tanimliyoruz nuray ismi siliniyor
+ console.log(name)*/
+
+ //let 
+ /*let name ='nuray'
+ console.log(name);
+ let name ='ayse';*/// burda name yeniden tanimlaya miyorsun
+
+/*let a,b;
+a=10;
+b=20;
+console.log(a+b);*/
+
+//const
+/*const name ='nuray';
+console.log(name);
+name = 'suat';// burda degeri degistiremiyorsun.
+console.log(name);*/
+
+//const a ;// degisikeni degeri burda vermelisin
+const a=[1,2,3,4,5];
+console.log(a);
+a.push('selam');// burda ekleme yapabiliriz ama yeni deger atamayiz.
+
+console.log(a);
