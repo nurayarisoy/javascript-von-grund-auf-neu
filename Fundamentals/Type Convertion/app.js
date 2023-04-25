b683cb908091c1b59bfeb8cbf77eabf882a29d77
Nuray arisoy <cevriyeceven@gmail.com>
2023-04-25 14:31:19 +0200

    variable conversion

---
+++ Fundamentals/Type Convertion/app.js
@@ -0,0 +1,28 @@
+/*let value;
+// veri typelerini stringe cevirme
+value =string(1234);
+value= string(3.14);
+value =string(true);
+value =string(false);
+value =string(function(){console.log()});
+value =string([1,2,3,4,]);
+
+value =(10).toString();
+value=(3.14).toString();
+
+//data type  number conversion
+value =Number('123');
+value =Number(null);
+value =Number(undefined);//sadece stringlerin icindekiler degisiyor yada null digerleri degismiyor
+value =Number('hello world');
+value =Number(function(){console.log()});
+ value =parseFloat('3.14');
+ value = parseInt('3');*/
+ //const a= 'hello' + 35;
+ const a =Number ('34')+ 53;// burda number olarak goruyor ve topluyor.
+ console.log(a);
+ console.log(typeof a);
+
+
+console.log(typeof value);
+console.log(value);
