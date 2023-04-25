d7ef79fd63b042f797e04c88dcbe4cf11eb80d41
Nuray arisoy <cevriyeceven@gmail.com>
2023-04-25 13:10:16 +0200

    datatype variabe

---
+++ Fundamentals/Variable,datatype/app.js
@@ -0,0 +1,56 @@
+//variable creation
+/*var a = 20;
+var b=10;
+var c =40;
+console.log(a,b,c);
+
+//primitive ilkel veri type sayi yazi undefeind null olabilir
+
+var a= 10;//burda otomatik number yapiyor
+console.log(a)
+console.log(typeof a);
+//stirig
+var name = 'nuray'
+console.log(name);
+console.log(typeof name);
+// boolean 
+var a =false
+console.log(typeof a)
+ var a = null;//burda null a hicbir deger tasimiyor
+
+var a;// undefind  herhangi bir deger verilmemis
+a=20;
+console.log(a)
+
+//reference data type array object dateobject ,function
+ var number =[1,2,3,4,5]
+ console.log(numbers);
+ console.log(typeof numbers); //burda obje olarak typeni belirliyor referance obje oluyor.
+ var person ={
+    name :'nuray arisoy',
+    age:40,
+    
+ }
+ console.log(person)
+ console.log(typeof person) */
+
+ var date = new Date();
+ console.log(date);
+ console.log(typeof date); 
+
+//prieme sadevce degisken olusturup deger veriyon  belekte degismiyor
+var a= 10
+console.log(a)
+var b= a;
+console.log(a,b)
+
+//reference  belekte degeri gösterir
+var a=[1,2,3,4];
+console.log(a)
+var b =a ;
+a.push(5);
+console.log(b)
+
+
+
+
