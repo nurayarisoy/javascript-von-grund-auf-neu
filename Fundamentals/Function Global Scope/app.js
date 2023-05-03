379e3cc0e6134137a64dbcf7d5649daf366cb67c
Nuray arisoy <cevriyeceven@gmail.com>
2023-05-03 15:24:18 +0200

    scope

---
+++ Fundamentals/Function Global Scope/app.js
@@ -0,0 +1,34 @@
+//scope kavrami
+
+//global scope  herhangibir if durumunda olmayan
+/*var value1 = 10;
+let value2 = 20;
+const value3 = 30;*/
+
+//function scope yazdik burda function icinde value olusuyor. diger globalde ayri valueler oluyor karismiyor.
+/*function Func() {
+    var value1=60;
+    let value2= 40;
+    const value3= 70;
+    console.log(value1,value2,value3)
+}
+Func();*/
+
+//block scope
+/*if (true) {
+  var a = 10; // burda sadece var blockdisinda tanimlarsin let ve const olmaz
+  let b = 20;
+  const c = 30;
+  console.log(a,b,c)
+}
+console.log(a); //disarda sadece a calisir degisir
+console.log(b);
+console.log(c);*/
+
+//console.log(value1, value2, value3);
+
+var datebase='2345676'; //global scope tanimladin
+ if (true) {
+    var database='9896564';
+    
+ }
