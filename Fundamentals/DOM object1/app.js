11a1deb2164bddea23c4f934d416ae4905da2607
Nuray arisoy <cevriyeceven@gmail.com>
2023-05-03 17:40:53 +0200

    document

--- Fundamentals/DOM object1/app.js
+++ Fundamentals/DOM object1/app.js
@@ -1,6 +1,19 @@
 let value;
 value = document;
-console.log(value);
+
 //script
 value = document.scripts;
 value = document.scripts.length;
+value =document.scripts[0];
+// linkler
+value =document.links;
+value =document.links[0];
+value =document.links[dokument.links.length-1];
+value =document.links[dokument.links.length-1].getAttribute('class');
+value =document.links[dokument.links.length-1].getAttribute('class');
+
+
+
+
+
+console.log(value);
