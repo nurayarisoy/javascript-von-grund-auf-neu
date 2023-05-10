939707793079a97c21f8f54172f530645da5b6c9
Nuray arisoy <cevriyeceven@gmail.com>
2023-05-10 20:45:24 +0200

    add style

--- Fundamentals/Element Style/app.js
+++ Fundamentals/Element Style/app.js
@@ -14,8 +14,23 @@ element.style.color = "#000"; //yazinin rengi koyulastir
 element.style.marginLeft = "50px";
 element.href = "https://arisoy-portfolio.netlify.app/";
 element.target = "blank";
-element.textContent='Löschen'
-element.textContent='löschen'
-element.innerHTML="<span style='color:white'>Löschen</span>"
+element.textContent = "Löschen";
+element.textContent = "löschen";
+element.innerHTML = "<span style='color:white'>Löschen</span>";
+/*const elements = document.querySelectorAll(".list-group-item"); //node list gelecek
+elements.forEach(function (el) {
+  el.style.color = "dark";
+  el.style.background = "#eee";
+}); *///m3scholl CSS selector istedigin child secersin istedigi li secmek  icin
+let element2 = document.querySelector("li:last-child");
+element2 = document.querySelector("li:nth-Child(2)");
+element2 = document.querySelector("li:nth-Child(3)");
+element2 = document.querySelector("li:nth-Child(4)");
+element2 = document.querySelector("li:nth-Child(odd)"); //tek elementler 3 5 elemanlar
+element2 = document.querySelectorAll("li:nth-Child(even)"); //cift child lari secersin
+element2.forEach(function (el) {
+  el.style.background = "#ccc";
+  el.style.color = "red";
+});
 
-console.log(element.textContent);
+console.log(element2);
