a5561245221652a60b6793e2612a6b8647e0a402
Nuray arisoy <cevriyeceven@gmail.com>
2023-05-10 22:01:46 +0200

    DOM element

---
+++ Fundamentals/DOM Element gezinti/app.js
@@ -0,0 +1,12 @@
+let value;
+const todoList = document.querySelector(".list-group");
+const todo = document.querySelector(".list-group-item:nth-child(2)");
+const cardrow = document.querySelector(".card.row");
+
+value = todoList;
+value = todo;
+value = cardrow;
+//child nodes
+value =todoList.childNodes;
+
+console.log(value);
