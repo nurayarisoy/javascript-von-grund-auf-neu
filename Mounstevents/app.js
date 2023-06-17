a5698ce0f6faf617162123b9717c32b9f2409f2c
Nuray arisoy <cevriyeceven@gmail.com>
2023-06-17 15:43:07 +0200

    Dom contendload

---
+++ Mounstevents/app.js
@@ -0,0 +1,28 @@
+//const cardBody =document.querySelectorAll('.card-body')[1];
+//const title =document.querySelector('#tasks-title')
+//clik Event
+//title.addEventListener('click'run);
+//douple click
+//title.addEventListener('dblclick',run);
+//mouse Down
+
+//title.addEventListener('maunsedown',run);
+//mouse up
+
+//title.addEventListener('maunseup',run);
+//mouse over
+//title.addEventListener('maunseover',run);
+//mouse Out
+//title.addEventListener('maunseout',run);
+// mouse enter Mouse leave
+//cardBody.addEventListener('mounseenter',run);
+//cardBody.addEventListener('mounseleave',run);
+//function run(e){
+//console.log(e.type);
+//}
+const filter = document.getElementById("filter");
+document.addEventListener("DOMcontendLoaded", load);
+
+function load(e) {
+  console.log("sayfa yüklendi");
+}
